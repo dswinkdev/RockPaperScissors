6f74e647529f928ae0f0ab9e68765497c3e036d5
Derricko Swink <dswinkgfx@gmail.com>
2025-12-10 07:27:19 -0600

    Gameplay intro

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -1,16 +1,29 @@
+import java.util.HashMap;
+import java.util.Random;
 import java.util.Scanner;
 
 public class RockPaperScissors extends Player{
-        boolean stillRunning = true;
 
+    boolean stillRunning = true;
+
+    int draw = 0;
+    int playerWins = 0;
+    int playerLosses = 0;
+    int cpuWins = 0;
+    int cpuLosses = 0;
+    int gameRound = 0;
 
         Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
         public RockPaperScissors(){}
 
         public RockPaperScissors(String playerName, double playerWins, double playerLosses, double totalWinsLosses) {
             super(playerName, playerWins, playerLosses, totalWinsLosses);
         }
+
+        String[] emojis = {"🚀","👽","💵","👀","⚡","️⭐","️👑","⚔️"};
+        //String[] emojiMoves = {"🪨","📃","✂️"};
 
         void menu() {
             System.out.println("- - - - - - - - - - - -");
@@ -24,6 +37,11 @@ public class RockPaperScissors extends Player{
         }
 
         void playGame() {
+            HashMap<Integer,String> emojiMoves = new HashMap<>();
+            emojiMoves.put(1,"🪨");
+            emojiMoves.put(2,"📄");
+            emojiMoves.put(3,"✂️");
+
             while (stillRunning) {
                 menu();
 
@@ -37,40 +55,111 @@ public class RockPaperScissors extends Player{
                 switch (userChoice) {
                     case 1 -> {
                         howToPlay();
-                        System.out.print("ready to play? (y/n): ");
-                        String readyToPlay = scanner.nextLine();
+                        System.out.print("\nready to play? (y/n): ");
+                        String readyToPlay = scanner.next();
 
                         if (readyToPlay.equalsIgnoreCase("y") || readyToPlay.equalsIgnoreCase("yes")){
                             createPlayer();
+                            gameRound++; // counts number of games before determining winner
+
+                            while (gameRound != 3) {
+                                System.out.print("choose (1) -> 🪨 Rock (2) -> 📃 Paper (3) -> ✂️: ");
+                            int userPick = scanner.nextInt();
+
+                            if (userPick < 1 || userPick > 3){
+                                System.out.println("invalid entry");
+                            } else {
+
+                                    int cpuPick = random.nextInt(1, 4);
+
+                                    // rock game logic
+                                    if (emojiMoves.get(userPick).equals("🪨") && emojiMoves.get(cpuPick).equals("🪨")) {
+                                        System.out.println("draw");
+                                        draw++;
+                                        System.out.println("You -> " + emojiMoves.get(userPick) + " | Cpu -> " + emojiMoves.get(cpuPick));
+                                    } else if (emojiMoves.get(userPick).equals("🪨") && emojiMoves.get(cpuPick).equals("📃")) {
+                                        System.out.println("paper covers rock");
+                                        System.out.println("Cpu wins!");
+                                        cpuWins++;
+                                        playerLosses++;
+                                        System.out.println("You -> " + emojiMoves.get(userPick) + " | Cpu -> " + emojiMoves.get(cpuPick));
+                                    } else if (emojiMoves.get(userPick).equals("🪨") && emojiMoves.get(cpuPick).equals("✂️")) {
+                                        System.out.println("rock beats scissors");
+                                        System.out.println("You win!");
+                                        playerWins++;
+                                        cpuLosses++;
+                                        System.out.println("You -> " + emojiMoves.get(userPick) + " | Cpu -> " + emojiMoves.get(cpuPick));
+                                    }
+
+                                    // paper game logic
+                                    if (emojiMoves.get(userPick).equals("📃") && emojiMoves.get(cpuPick).equals("📃")) {
+                                        System.out.println("draw");
+                                        draw++;
+                                    } else if (emojiMoves.get(userPick).equals("📃") && emojiMoves.get(cpuPick).equals("🪨")) {
+                                        System.out.println("paper covers rock");
+                                        System.out.println("You win!");
+                                        playerWins++;
+                                        cpuLosses++;
+                                    } else if (emojiMoves.get(userPick).equals("📃") && emojiMoves.get(cpuPick).equals("✂️")) {
+                                        System.out.println("scissors cut paper");
+                                        System.out.println("You loss!");
+                                        cpuWins++;
+                                        playerLosses++;
+                                    }
+
+                                    // scissors game logic
+                                    if (emojiMoves.get(userPick).equals("✂️") && emojiMoves.get(cpuPick).equals("✂️")) {
+                                        System.out.println("draw");
+                                        draw++;
+                                    } else if (emojiMoves.get(userPick).equals("✂️") && emojiMoves.get(cpuPick).equals("🪨")) {
+                                        System.out.println("rock beats scissors");
+                                        System.out.println("Cpu wins!");
+                                        cpuWins++;
+                                        playerLosses++;
+                                    } else if (emojiMoves.get(userPick).equals("✂️") && emojiMoves.get(cpuPick).equals("📃")) {
+                                        System.out.println("scissors cut paper");
+                                        System.out.println("You win!");
+                                        playerWins++;
+                                        cpuLosses++;
+                                    }
+                                }
+
+                            }
                         } else if (readyToPlay.equalsIgnoreCase("n") || readyToPlay.equalsIgnoreCase("no")){
-                            return;
+                        return;
                         } else {
                             System.out.println("invalid entry");
                         }
+                    }
+                    case 2 -> createPlayer();
+                    case 3 -> playGame();
+                    case 4 -> {
+                        System.out.println("exiting game...");
+                        return;
                     }
                 }
             }
         }
 
         void createPlayer() {
-            System.out.println("- - - 👽Create Player - - -");
-            System.out.println("enter name: ");
+            System.out.println("\n- - - 👽Create Player - - -");
+            System.out.print("enter name: ");
+            scanner.nextLine();
             String userName = scanner.nextLine();
 
-            while (true) {
-                if (userName.isEmpty() || userName.length() > 15) {
+                if (userName.length() < 3 || userName.length() > 15) {
                     System.out.println("invalid username");
-                }
+                } else {
+                    setPlayerName(userName);
+                    System.out.println("👽" + userName);
 
-                System.out.println("👽" + userName);
-                System.out.println("\nloading " + userName + "'s stats...");
-                System.out.println(userName + ": W " + getPlayerWins() + " | L " + getPlayerLosses());
-            }
+
+                }
         }
 
         void howToPlay() {
             System.out.println(
-                    "How to Play:\n" +
+                    "\nHow to Play:\n" +
                             "1. When prompted, enter rock, paper, or scissors.\n" +
                             "2. The computer will randomly choose its move.\n" +
                             "3. The program compares both choices and determines the winner:\n" +
